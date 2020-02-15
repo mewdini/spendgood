27ca190958dd0003391ed3d56bce9a783d8495b6
Pablo Solano <pasolano@email.wm.edu>
2020-02-15 17:25:37 -0500

    move top_spending to scripts

---
+++ test.sh
@@ -0,0 +1,12 @@
+curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{
+  \"merchant_id\": \"57cf75cfa73e494d8675f928\",
+  \"medium\": \"balance\",
+  \"purchase_date\": \"2020-02-15\",
+  \"amount\": 3
+}" "http://api.reimaginebanking.com/accounts/5e481a2cf1bac107157e0abd/purchases?key=cb456762a68ec02541768d65d2cfa5e3"
+curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{
+  \"merchant_id\": \"57cf75cfa73e494d8675f928\",
+  \"medium\": \"balance\",
+  \"purchase_date\": \"2020-02-15\",
+  \"amount\": 3
+}" "http://api.reimaginebanking.com/accounts/5e481a2cf1bac107157e0abd/purchases?key=cb456762a68ec02541768d65d2cfa5e3"
