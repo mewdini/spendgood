566df27e367ea9c4737c552abbb6a9b349bc7e0a
Pablo Solano <pasolano@email.wm.edu>
2020-02-15 14:34:16 -0500

    more efficient coffee calculation

--- coffee.py
+++ coffee.py
@@ -8,9 +8,7 @@ user_account = "5e481a2cf1bac107157e0abd"
 
 key = 'cb456762a68ec02541768d65d2cfa5e3'
 url = 'http://api.reimaginebanking.com/merchants?key={}'.format(key)
-response = requests.get(
-    url
-)
+response = requests.get(url)
 
 ids = []
 merchant_data = response.json()
@@ -20,12 +18,37 @@ for merchant in merchant_data:
             if category == "cafe":
                 ids.append(merchant["_id"])
 
+url = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(user_account, key)
+response = requests.get(url)
+purchases = []
+purchase_data = response.json()
+total = 0
+
+for purchase in purchase_data:
+    if "merchant_id" in purchase:
+        if purchase["merchant_id"] in ids:
+            if "amount" in purchase:
+                total = total + purchase["amount"]
+
+print(total)
+
+
+'''
+#get purchases user made at cafes
+total = 0
 for id in ids:
     url = "http://api.reimaginebanking.com/merchants/{}/accounts/{}/purchases?key={}".format(id, user_account, key)
     response = requests.get(
         url
     )
-    
+    purchase_data = response.json()
+    for purchase in purchase_data:
+        if "amount" in purchase:
+            total = total + purchase["amount"]
+
+print(total)
+'''
+
 
 
 average = 3.16 #hardcoded average
