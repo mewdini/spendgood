e629d7499685c0de9a4eccf7c137abeda45cce7d
ahfeng <ahfeng@email.wm.edu>
2020-02-15 15:11:44 -0500

    dict of census category:annual spending per adult

--- scripts/list_merchant_types.py
+++ scripts/list_merchant_types.py
@@ -1,10 +1,13 @@
 import numpy as np
-#import pandas as pd
+import pandas as pd
 import requests
 import json
 
+US_ADULT_POPULATION_2017=251564106
+RCP_SCALE = 1000
 customerId = '5e47858a322fa016762f38ec'
 apiKey = 'e505195e938067f8af2c632e14b59140'
+fpath = '../data/ECNBASIC2017.EC1744BASIC_data_with_overlays_2020-02-15T140430.csv'
 
 url = 'http://api.reimaginebanking.com/merchants?key={}'.format(apiKey)
 
@@ -26,16 +29,17 @@ if response:
             if type(merchant_category) == list:
                 merchant_categories = merchant_category
                 for category in merchant_categories:
-                    category = category.lower().encode('ascii','ignore')
-                    # if category == 'rip-offs':
+                    category = category.lower()#.encode('ascii','ignore')
+                    # if category == 'porn':
                     #     print(merchant)
                     if category not in categories:
                         categories.append(category)
                     #category = category[1:2]
                     
             else:
-                merchant_category = merchant_category.lower().encode('ascii','ignore')
-                
+                merchant_category = merchant_category.lower()#.encode('ascii','ignore')
+                # if merchant_category == 'porn':
+                #     print(merchant)
                 if merchant_category not in categories:
                         categories.append(merchant_category)
 
@@ -46,3 +50,15 @@ if response:
     print(categories)
 else:
     print('An error has occurred.')
+with open(fpath, 'r') as f:
+    df = pd.read_csv(f)
+    #print(df['NAICS2017_LABEL'].head)]
+    census_labels = {}
+    for index, row in df[['NAICS2017','NAICS2017_LABEL','RCPTOT']][2:].iterrows():
+        if row['NAICS2017_LABEL'] not in census_labels:
+            census_labels[row['NAICS2017_LABEL']]=float(row['RCPTOT'])*RCP_SCALE/US_ADULT_POPULATION_2017
+    #for entry in df[2:]:
+        #print(entry)
+        # if entry['NAICS2017_LABEL'] not in census_labels:
+        #     census_labels[entry['NAICS2017_LABEL']]=entry['NAICS2017']
+    print(census_labels)
