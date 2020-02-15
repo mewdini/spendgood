4c435f2e29da6d4f2a7a13793b02201002fe8b2a
Pablo Solano <pasolano@email.wm.edu>
2020-02-15 16:33:27 -0500

    new coffee algorithm

--- coffee.py
+++ coffee.py
@@ -3,53 +3,25 @@
 import json
 import requests
 
-user_id = "5e47855f322fa016762f38e3"
 user_account = "5e481a2cf1bac107157e0abd"
+key = "cb456762a68ec02541768d65d2cfa5e3"
 
-key = 'cb456762a68ec02541768d65d2cfa5e3'
-url = 'http://api.reimaginebanking.com/merchants?key={}'.format(key)
-response = requests.get(url)
-
-ids = []
-merchant_data = response.json()
-for merchant in merchant_data:
-    if "category" in merchant:
-        for category in merchant["category"]:
-            if category == "cafe":
-                ids.append(merchant["_id"])
-
+# get purchases
 url = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(user_account, key)
-response = requests.get(url)
-purchases = []
-purchase_data = response.json()
-total = 0
+purchases = requests.get(url)
+purchases_data = purchases.json()
 
-for purchase in purchase_data:
+totals = {}
+for purchase in purchases_data:
     if "merchant_id" in purchase:
-        if purchase["merchant_id"] in ids:
-            if "amount" in purchase:
-                total = total + purchase["amount"]
-
-print(total)
-
-
-'''
-#get purchases user made at cafes
-total = 0
-for id in ids:
-    url = "http://api.reimaginebanking.com/merchants/{}/accounts/{}/purchases?key={}".format(id, user_account, key)
-    response = requests.get(
-        url
-    )
-    purchase_data = response.json()
-    for purchase in purchase_data:
-        if "amount" in purchase:
-            total = total + purchase["amount"]
-
-print(total)
-'''
-
-
-
-average = 3.16 #hardcoded average
-
+        merchant_id = purchase["merchant_id"]
+        url = 'http://api.reimaginebanking.com/merchants/{}?key=cb456762a68ec02541768d65d2cfa5e3'.format(merchant_id)
+        merchant = requests.get(url) #get category
+        merchant_data = merchant.json()
+        for category in merchant_data["category"]:
+            if category in totals:
+                totals[category] += float(purchase["amount"])
+            else:
+                totals[category] = float(purchase["amount"])
+totals = {k: v for k, v in sorted(totals.items(), key=lambda item: item[1], reverse=True)}
+print(totals)
